d6712d62c930a91e3ee88ac11056bd3c0a07f14b
Sergey Sirotin <sergst152@gmail.com>
2022-10-20 16:56:53 +0300

    add web_service_check.sh

---
+++ web_service_check.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Возвращение вывода к стандартному форматированию
+NORMAL='\033[0m'
+
+# Цветом текста (жирным) (bold) :
+WHITE='\033[1;37m'
+
+# Цвет фона
+BGRED='\033[41m'
+BGGREEN='\033[42m'
+BGBLUE='\033[44m'
+
+nginxstatus=$(systemctl status nginx | grep -Eo "running|failed|dead")
+if [[ $nginxstatus == "running" ]]
+  then
+	  echo -en "${WHITE} ${BGGREEN} web server NGINX is running $(curl -sI 192.168.0.13 | head -n 1 | cut -d $" " -f 2,3,4,5,6) ${NORMAL}\n"
+  else
+    echo -en "${WHITE} ${BGRED} web server NGINX not running $(curl -sI 192.168.0.13 | head -n 1 | cut -d $" " -f 2,3,4,5,6) ${NORMAL}\n"
+    systemctl restart nginx
+    sleep 1
+    echo -en "${WHITE} ${BGGREEN} NGINX web server status $(systemctl status nginx | grep -Eo "running|failed|dead") after restart $(curl -sI 192.168.0.13 | head -n 1 | cut -d $" " -f 2,3,4,5,6) ${NORMAL}\n"
+fi
